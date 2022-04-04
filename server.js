ab794b66813f66f2dce3f242e025536ac1449bd7
tnichols <tnichols@student.dalat.org>
2022-04-04 18:24:04 +0800

    change port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('*', function (req, res) {
     }
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
 
